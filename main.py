204dc23ff1a62b5b9a1dba0d1deb4b3a0e53082c
ftwasdYK <ikaraparisis@gmail.com>
2025-06-30 18:55:30 +0300

    build the generate endpoint

--- main.py
+++ main.py
@@ -3,26 +3,30 @@ import uvicorn
 from pydantic import BaseModel
 import numpy as np
 import torch
-
 from typing import List
 
 from training.models import CustomResNet50
-from utils_app import pipeline_prediction
-import os #<--- this
+from utils_app import pipeline_prediction, sample_ddpm
 
 app = FastAPI()
 
 # load model
-# checkpoint_path = os.environ.get("CHECKPOINT_PATH", "./checkpoints/CustomResNet50/ckp_epoch_2.pth") #<--- this
-checkpoint = torch.load('./checkpoints/CustomResNet50/ckp_epoch_2.pth')
-NN_MODEL= CustomResNet50(pretrained=False)
-NN_MODEL.load_state_dict(checkpoint['model_state_dict'])
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+CKP = torch.load('./checkpoints/CustomResNet50/ckp_epoch_20.pth', map_location=torch.device(DEVICE))
+NN_MODEL= CustomResNet50(pretrained=False).to(DEVICE)
+NN_MODEL.load_state_dict(CKP['model_state_dict'])
 
 class ImageRequest(BaseModel):
     img_raw: List[List[float]]
 
 class PredictionResponse(BaseModel):
     prediction: int
+
+class NumberRequest(BaseModel):
+    number: int
+ 
+class ImageResponse(BaseModel):
+    img_list: List[List[int]]
 
 pipeline = pipeline_prediction(NN_MODEL)
 
@@ -38,6 +42,13 @@ def predict(image: ImageRequest) -> PredictionResponse:
     data = pipeline.preprocess_image(np.array(data))
     pred = pipeline.make_predictions(data)
     return PredictionResponse(prediction=pred)
+
+@app.post("/generate")
+def generate(number: NumberRequest) -> ImageResponse:
+    num = number.number
+    img = sample_ddpm(num)
+    img_list = img.cpu().numpy().tolist()
+    return ImageResponse(img_list=img_list)
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
