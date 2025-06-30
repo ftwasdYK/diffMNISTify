02a9cf095ba688db18d897c274c0fe40b55e7ce1
ftwasdYK <ikaraparisis@gmail.com>
2025-06-30 19:20:21 +0300

    add missing directories

--- build_project.sh
+++ build_project.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ ! -d "checkpoints/CustomResNet50" ]; then
+    mkdir -p checkpoints/CustomResNet50
+fi
+
+if [ ! -d "checkpoints/DDPM_conditional/" ]; then
+    mkdir -p checkpoints/DDPM_conditional
+fi
+
 # Check if argument is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <absolute_path_to_directory>"
