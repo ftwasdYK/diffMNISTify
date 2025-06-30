0fd689680e16350bf716bad9c8bc5813a7aaf516
ftwasdYK <ikaraparisis@gmail.com>
2025-06-30 11:50:12 +0300

    feat: support training and testing with synthetic data

--- training/train_eval_pipeline_nn.py
+++ training/train_eval_pipeline_nn.py
@@ -11,26 +11,24 @@ from training_loops import MultiClassificationTrainer
 import pandas as pd
 import os
 
-print("Running from directory:", os.getcwd())
-
 if not os.path.exists("./training/results"):
         os.makedirs("./training/results")
 
 if not os.path.exists("./training/figures"):
         os.makedirs("./training/figures")
 
-
 BATCH_SIZE = 16
 EPOCHS = 2
 LR = 1e-2
 WEIGHT_DECAY = 1e-5
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-NN_MODEL = CustomResNet50().to(DEVICE) # CustomViT()# 
-print('Total trainable parameters', NN_MODEL.count_total_parameters())
+NN_MODEL = CustomResNet50(pretrained=False).to(DEVICE) # CustomViT().to(DEVICE)# 
+GEN_DATA = True
 # Optimizer
 optimizer = torch.optim.AdamW(NN_MODEL.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
 loss_fn = torch.nn.CrossEntropyLoss()
 scheduler = StepLR(optimizer, step_size=5, gamma=0.5) 
+print('Total trainable parameters', NN_MODEL.count_total_parameters())
 
 
 transform_train = v2.Compose([
@@ -40,6 +38,7 @@ transform_train = v2.Compose([
         v2.RandomHorizontalFlip(p=0.2),
         v2.RandomRotation(degrees=30),
         v2.RandomVerticalFlip(p=0.2),
+        v2.GaussianNoise()
     ])
 
 transform_inference = v2.Compose([
@@ -51,7 +50,8 @@ transform_inference = v2.Compose([
 train_loader, val_loader, test_loader = prepare_data(
     transform_tr=transform_train,
     transform_infer=transform_inference,
-    batch_size=BATCH_SIZE
+    batch_size=BATCH_SIZE,
+    gen_data=GEN_DATA
 )
 
 nn_m = MultiClassificationTrainer(
@@ -63,13 +63,13 @@ nn_m = MultiClassificationTrainer(
     EPOCHS,
     DEVICE,
     scheduler=scheduler,
-    verbose=True
+    verbose=True,
+    gen_data=GEN_DATA
 )
 
 hist, checkp_dir = nn_m.train_nn()
 
 # load the best model
-# checkp_dir = './checkpoints/CustomResNet50/ckp_epoch_2.pth'
 nn_m.load_checkpoint(checkp_dir)
 
 ##### Evaluation Phase #####   
@@ -81,7 +81,8 @@ results_df = pd.DataFrame(results)
 print(conf_matrix_df)
 print(results_df)
 prefix = f'{NN_MODEL.__class__.__name__}_lr{LR}_wd{WEIGHT_DECAY}_bs{BATCH_SIZE}_epochs{EPOCHS}'
-results_df.to_csv(f"./training/results/test_results_{prefix}.csv", index=False)
-conf_matrix_df.to_csv(f"./training/results/confusion_matrix_{prefix}.csv", index=False)
+gen_dir = 'gen_data' if GEN_DATA else ''
+results_df.to_csv(f"./training/results/test_results_{prefix}_{gen_dir}.csv", index=False)
+conf_matrix_df.to_csv(f"./training/results/confusion_matrix_{prefix}_{gen_dir}.csv", index=False)
 
-save_confusion_matrix_plot(conf, f'./training/figures/confusion_matrix_{prefix}.png')
+save_confusion_matrix_plot(conf, f'./training/figures/confusion_matrix_{prefix}_{gen_dir}.png')
